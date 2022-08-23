a3df43ab15bfecb8dab29b463bb0ce65a3e44f12
hongkpng854 <99008862+hongkpng854@users.noreply.github.com>
2022-08-23 21:08:37 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWebUnity2.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWebUnity3.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
